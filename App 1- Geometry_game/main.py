070f81da9d9c02e5ea43f24f5b8ff70de17a5232
marybello <58462503+marybello@users.noreply.github.com>
2021-11-30 22:12:45 +0100

    Add files via upload

---
+++ App 1- Geometry_game/main.py
@@ -0,0 +1,15 @@
+import turtle
+
+
+myturtle = turtle.Turtle()
+
+
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
